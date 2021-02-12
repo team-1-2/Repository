930a3ce1bc3f779277d8891f12db528410859f1f
Mark Alex Xbox <65962153+Blue-Sea2@users.noreply.github.com>
2021-02-12 13:21:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
+//function install(file,type){
+//  download(file);
+//  delay(10000);
+//  window.open(type+".html","_self");
+//}
 function install(file,type){
-  download(file);
-  delay(10000);
-  window.open(type+".html","_self");
+  let myPromise = new Promise(function(myResolve, myReject) {
+
+    download(file);
+
+
+  myResolve(){window.open(type+".html","_self");}; // when successful
+
+  myReject(){install(file, type);};  // when error
 }
