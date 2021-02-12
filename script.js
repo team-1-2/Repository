1aa6c1d67e4514af48c5b1fe1ff87be100dc0042
Mark Alex Xbox <65962153+Blue-Sea2@users.noreply.github.com>
2021-02-12 14:21:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,12 +4,26 @@
 //  window.open(type+".html","_self");
 //}
 function install(file,type){
-  let myPromise = new Promise(function(myResolve, myReject) {
+ let myPromise = new Promise(function(myResolve, myReject) {
 
-    download(file);
+  download(file);
 
 
-  myResolve(){window.open(type+".html","_self");}; // when successful
 
-  myReject(){install(file, type);};  // when error
+  myResolve(); // when successful
+
+  myReject();  // when error
+
+});
+
+
+// "Consuming Code" (Must wait for a fulfilled Promise)
+
+myPromise.then(
+
+  function(value) { window.open(type+".html","_self"); },
+
+  function(error) { install(file,type); }
+
+);
 }
