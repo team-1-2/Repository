c2ec6cff6fe90fba6aa636971bd933430e126917
Mark Alex Xbox <65962153+Blue-Sea2@users.noreply.github.com>
2021-02-24 08:26:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,10 @@ function openURL(URL) {
 function home() {
 	window.open("index.html", "_self");
 }
+function blocks() {
+	const block1 = getElementById("block1");
+	var autoClicker = "<a href='https://team-1-2.github.io/Repository/autoclicker.html'><p>New on repository! Auto Clicker!</p></a>";
+	var prodigyHack = "<a href='https://team-1-2.github.io/Repository/prodigyHack.html'><p>New on repository! Prodigy Hack!</p></a>";
+	var blocks = [autoClicker, prodigyHack];
+	block1.innerHTML = blocks[blocks.length];
+}
