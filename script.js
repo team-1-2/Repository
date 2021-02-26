bc2159a2fe6b7b409df3b8a059c17628f7f161de
Mark Alex Xbox <65962153+Blue-Sea2@users.noreply.github.com>
2021-02-26 19:10:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,20 +11,6 @@ function home() {
 function say(message) {
 	alert(message);
 }
-function requestFullScreen(element) {
-    // Supports most browsers and their versions.
-    var requestMethod = element.requestFullScreen || element.webkitRequestFullScreen || element.mozRequestFullScreen || element.msRequestFullScreen;
-
-    if (requestMethod) { // Native full screen.
-        requestMethod.call(element);
-    } else if (typeof window.ActiveXObject !== "undefined") { // Older IE.
-        var wscript = new ActiveXObject("WScript.Shell");
-        if (wscript !== null) {
-            wscript.SendKeys("{F11}");
-        }
-    }
-}
-function click(element) {
-	var elem = document.getElementById(element); 
-	elem.click();
+function fullscreen(URL) {
+ 	window.open(theURL, '', '_self, fullscreen=yes');
 }
