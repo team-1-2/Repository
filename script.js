b9e220e4d71603de4faffcc555c303b86ffe3438
Mark Alex Xbox <65962153+Blue-Sea2@users.noreply.github.com>
2021-03-02 08:00:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -34,3 +34,17 @@ function click(element) {
 		click(element);
 	}
 }
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("visible");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
