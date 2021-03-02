ffe177d9ea1b9df2924f44fd31d612ac15cb176e
Mark Alex Xbox <65962153+Blue-Sea2@users.noreply.github.com>
2021-03-02 17:14:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -38,14 +38,14 @@ var coll = document.getElementsByClassName("collapsible");
 var i;
 
 for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("visible");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-	coll.style.display = "none";
-    }
-  });
+	coll[i].addEventListener("click", function() {
+		this.classList.toggle("visible");
+		var content = this.nextElementSibling;
+		if (content.style.display === "block") {
+			content.style.display = "none";
+		} else {
+			content.style.display = "block";
+		}
+		coll[i].style.display = "none";
+	});
 }
